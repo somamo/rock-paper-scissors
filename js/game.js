049ee64153d22b7c1fcbd261cfad27f35ea3709d
somamo <kadtmail@gmail.com>
2016-02-01 10:21:33 +0100

    fix IE compatibility

--- js/game.js
+++ js/game.js
@@ -10,12 +10,11 @@ var computerscoreDisplay = document.querySelector(".computer-score");
 var reset = document.querySelector(".reset");
 
 reset.addEventListener("click", resetScore, false);
-rock.addEventListener("click", comparerock, false);
-paper.addEventListener("click", comparepaper, false);
-scissors.addEventListener("click", comparescissors, false);
+rock.addEventListener("click", function() {compare("rock", computerChoice());}, false);
+paper.addEventListener("click", function() {compare("paper", computerChoice());}, false);
+scissors.addEventListener("click", function() {compare("scissors", computerChoice());}, false);
 
 function computerChoice() {
-
     var computerChoice = Math.random();
 
     if (computerChoice < 0.34) {
@@ -27,7 +26,8 @@ function computerChoice() {
     }
 };
 
-function comparerock(ev) {
+
+/*function comparerock(ev) {
     compare("rock", computerChoice());
 };
 
@@ -37,7 +37,7 @@ function comparepaper(ev) {
 
 function comparescissors(ev) {
     compare("scissors", computerChoice());
-};
+};*/
 
 function compare(userChoice, computerChoice) {
     var resultreason = document.querySelector(".result-reason");
@@ -104,11 +104,11 @@ function toggleArea() {
     var counter = document.querySelector(".counter");
     var result = document.querySelector(".results");
 
-    if (!result.className.includes("hidden")){
+    if (result.className.indexOf("hidden") < 0){
         result.className = "results hidden";
         counter.className = "counter hidden";
         shapes.className = "shapes";
-    } else if (!counter.className.includes("hidden")){
+    } else if (counter.className.indexOf("hidden") < 0){
         shapes.className = "shapes hidden";
         counter.className = "counter hidden";
         result.className = "results";
